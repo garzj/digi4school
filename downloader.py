8446f39528027fd73cc5a817ebb4eb05bf22b457
garzj <jojogarz123@gmail.com>
2020-06-19 02:16:12 +0200

    Added conversion to png/pdf/ps/png

--- downloader.py
+++ downloader.py
@@ -2,74 +2,79 @@ import requests
 from pathlib import Path
 from bs4 import BeautifulSoup
 from time import sleep
+import cairosvg_
 
 # get some options from the user
 print('')
 bookUrl = input('Paste your book url: ')
-pageNo = int(input('Start downloading from page (1): ') or 1)
+startPageNo = int(input('Start downloading from page (default => 1): ') or 1)
+pageFileExtension = input('Choose a file extension for the pages: png/pdf/ps/svg (default => png): ') or 'png'
 
 # Starting message
 print('')
 print('>>> Starting book download...')
 
 # extract a valid bookId from the url
-bookId = -1
+bookIds = []
 for tile in bookUrl.split('/'):
     try:
-        bookId = int(tile)
+        id = int(tile)
+        bookIds.append(id)
     except ValueError:
         pass
-if bookId == -1:
+if len(bookIds) == 0:
     print("<<< Error: The book url you provided seems to be invalid.")
     exit()
 
-# setup directory for the book
-bookDir = f'./downloads/book-{bookId}/'
-Path(bookDir).mkdir(parents=True, exist_ok=True)
+# extrace the book suburl from the given ids
+bookSubUrl = ''
+for bookId in bookIds:
+   bookSubUrl += '/' + str(bookId)
 
 # setup an authorized session for the download (we need to make some requests here to get a valid token)
-session = requests.Session()
-def getFormData(html):
-    form = BeautifulSoup(html, features='lxml').findAll('form')[0]
-    formdata = {}
-    for field in form:
-        if (field.has_attr('name') and field.has_attr('value')):
-            formdata[field['name']] = field['value']
-    return formdata
-r = session.get(f'https://digi4school.at/token/{bookId}')
-r = session.post('https://kat.digi4school.at/lti', data=getFormData(r.content))
-r = session.post('https://a.digi4school.at/lti', data=getFormData(r.content))
+try:
+    session = requests.Session()
+    def getFormData(html):
+        form = BeautifulSoup(html, features='lxml').findAll('form')[0]
+        formdata = {}
+        for field in form:
+            if (field.has_attr('name') and field.has_attr('value')):
+                formdata[field['name']] = field['value']
+        return formdata
+    r = session.get(f'https://digi4school.at/token/{bookIds[0]}')
+    r = session.post('https://kat.digi4school.at/lti', data=getFormData(r.content))
+    r = session.post('https://a.digi4school.at/lti', data=getFormData(r.content))
+except IndexError:
+    print("<<< Error: The book url you provided seems to be invalid.")
+    exit()
 
-# loop as long as a valid pages are found
-ddosMessageShown = False
-lastWasPage = True
-while lastWasPage:
+# mkdir a directory for the book
+if len(bookIds) == 1:
+    bookDir = f'./downloads/book-{bookIds[0]}/'
+elif len(bookIds) == 2:
+    bookDir = f'./downloads/archive-{bookIds[0]}/book-{bookIds[1]}/'
+else:
+    bookDir = f'./downloads{bookSubUrl}/'
+Path(bookDir).mkdir(parents=True, exist_ok=True)
+
+# go on and keep downloading the pages ´til there's a 404
+pageNo = startPageNo
+while True:
+    # download the page, convert it to the given file extension
+    pageSvgUrl = f'https://a.digi4school.at/ebook{bookSubUrl}/{pageNo}/{pageNo}.svg'
+    fileSavePath = f'./{bookDir}/page-{pageNo}.{pageFileExtension}'
     try:
-        # download the page
-        pageSvgUrl = f'https://a.digi4school.at/ebook/{bookId}/1/{pageNo}/{pageNo}.svg'
-        r = session.get(pageSvgUrl)
-        lastWasPage = (r.status_code == 200 and 'image/svg' in r.headers['content-type'])
-        if lastWasPage:
-            filePath = f'./{bookDir}/page-{pageNo}.svg'
-            with open(filePath, 'wb') as f:
-                f.write(r.content)
-                print(f'Downloaded page {pageNo}.')
-                pageNo += 1
-    except requests.exceptions.RequestException as e:
-        # handle DDOS protection errors
-        if not ddosMessageShown:
-            print("")
-            print("Oops! Looks like have been temporarily blocked by the digi4school server's DDOS protection.")
-            print("This normally happens when you send too many requests (download large books).")
-            print("")
-            print(f"If you want to try it again later, here's the next page to be downloaded: {pageNo}")
-            print("")
-            print("The thread will automatically retry every 10 seconds ...")
-            print("")
-            ddosMessageShown = True
+        if pageFileExtension in cairosvg_.svg2:
+            cairosvg_.svg2[pageFileExtension](url=pageSvgUrl, write_to=fileSavePath, session=session, fillBG=True)
+            print(f'Downloaded page {pageNo}.')
+            pageNo += 1
         else:
-            print("Error, retrying...")
-        sleep(10)
+            print("<<< Error: Please enter a valid file extension.")
+            exit()
+            break
+    except cairosvg_.exceptions.NotFoundException:
+        # 404 -> we're done
+        break
 
-# Success message
-print(f'<<< Downloaded {pageNo - 1} pages.')
+# Done message
+print(f'<<< Downloaded {pageNo - startPageNo} pages.')
